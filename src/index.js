65dc4a5ae4d5500f6bbdee92f35e55215f24cb17
Carlos Miceli <carlos@adbadger.com>
2022-07-29 17:03:06 -0500

    project started

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Title from './components/Title.tsx';
+import CardContainer from './components/CardContainer.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Title>Carlos' Daily Stuff</Title>
+    <CardContainer title='Spotify Recommendation'/>
   </React.StrictMode>
 );
 
