2ef38e24055fcc81b4d01503d3cb5ed3589498cc
carlosmiceli <carlosmiceli87@gmail.com>
2023-07-25 16:40:30 -0300

    hosting BE/FE

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'http://localhost:8000/cards',
+	baseURL: 'https://frolicking-taffy-108122.netlify.app/cards',
 	headers: {
 		'Content-type': 'application/json',
 	},
